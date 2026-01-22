474a61392c31cb9c0f783c92e083e975cd37441d
CodeLazarus <draganstamenkovic@outlook.com>
2026-01-22 22:50:01 +0100

    Updated version of Fox World for WebGL

---
+++ projects/Fox World/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Dragan Stamenkovic-Fox World-1.0";
+const contentToCache = [
+    "Build/Fox World.loader.js",
+    "Build/Fox World.framework.js",
+    "Build/Fox World.data",
+    "Build/Fox World.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
